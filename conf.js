bded4fdfd8a9ade8942d03847f244cdeea63c135
Flávio Meira <flaviomeira10@gmail.com>
2016-02-27 17:59:15 -0300

    subindo travis yml

--- conf.js
+++ conf.js
@@ -1,5 +1,8 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
+  //seleniumServerJar: 'null' ,
   specs: ['todo-spec.js'],
-  baseUrl: 'http://localhost:9876'
+  baseUrl: 'http://localhost:9876',
+  sauceUser: 'flaviomeira10',
+  sauceKey: '2e2fe2d0-1d8f-4296-be0c-66881f07b96b'
 };
--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,16 @@ var
 	gulp = require('gulp');
 
 gulp.task('protractor-run', function (done) {
-	return gulp.src(["./src/tests/*.js"])
+	return gulp.src(["*.js"])
 	.pipe(protractor({
 		configFile: "conf.js",
 		args: ['--baseUrl', 'https://angularjs.org']
 	}))
 	.on('error', function(e) { throw e });
+});
+
+gulp.task('run-dev', function(done){
+	return 
 });
 
 gulp.task('default', ['protractor-run']);
